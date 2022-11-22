4493b32239492bdd87e758a5dffe00b3abd90d83
Diana-Cano <dianacanoo@icloud.com>
2022-11-22 15:21:18 -0800

    adding 404 page view

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get('/',(req,res)=> {
     res.render('Home')
 })
 
-app.get('*',(requ,res)=>{
-    res.status(404).send('<h1>404 Page</h1>')
+app.get('*',(req,res)=>{
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
