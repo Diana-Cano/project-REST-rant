8a1fbe11389c92b98a9ebd70bcad9e818633912f
Diana-Cano <dianacanoo@icloud.com>
2022-11-29 15:41:23 -0800

    Show route connected

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -18,10 +18,13 @@ router.post('/', (req, res) => {
 })
 
 
+
+
 //get/places/new
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
 
 // GET /places
 router.get('/', (req,res)=>{
@@ -29,7 +32,19 @@ router.get('/', (req,res)=>{
   res.render('places/index', {places})
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
 
+  }
+})
 
 
   
