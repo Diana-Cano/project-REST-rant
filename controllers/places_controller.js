b42740d2441fbd3e8aac963c10924e0105174a29
Diana-Cano <dianacanoo@icloud.com>
2022-11-28 21:28:54 -0800

    new places can be added

---
+++ controllers/places_controller.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const places = require ('../models/places.js')
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+//get/places/new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+// GET /places
+router.get('/', (req,res)=>{
+ 
+  res.render('places/index', {places})
+})
+
+
+
+
+  
+module.exports = router
+
