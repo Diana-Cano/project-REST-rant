e67dd84ed49d39ba014e3f96f715fcc12fc6fa24
Diana-Cano <dianacanoo@icloud.com>
2022-12-06 19:31:15 -0800

    added mongo db, created schema for places, still need to fix required om cuisine ?

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -27,6 +27,11 @@ function new_form () {
                      <label htmlFor="cuisine">Cuisine</label>
                      <input className = 'form-control' type="cuisine" id="cuisine" name="cuisine" required/>
                 </div>
+                <div className="form-group">
+                      <label for="founded">Founded Year</label>
+                      <input className="form-control" id="founded" name="founded" />
+                </div>
+
                 <input className = 'btn btn-primary' type = 'submit' value = 'add place'/>
             </form>
           </main>
