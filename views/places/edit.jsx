81420c32f8090300ca127e8509c64451c2811bde
Diana-Cano <dianacanoo@icloud.com>
2022-12-06 18:30:30 -0800

    completer part 7, added edit and rendered updated edit page

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,33 +1,33 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form (data) {
+function edit_form ({place, id}) {
     return (
         <Def>
           <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+            <form method="POST" action={`/places/${id}?_method=PUT`}>
                 <div className ='row'>
                     <div className = "form-group col-sm-6">
                     <label htmlFor="name">Place Name</label>
-                    <input className = 'form-control' id='name' name = 'name' value = {data.place.name}required/>
+                    <input className = 'form-control' id='name' name = 'name' value = {place.name}required/>
                     </div>
                 <div className="form-group col-sm-6">
                      <label htmlFor="pic">Place Picture</label>
-                     <input className = 'form-control' type="url" id="pic" name="pic" value = {data.place.pic} />
+                     <input className = 'form-control' type="pic" id="pic" name="pic" value = {place.pic} />
                 </div>
                 </div>
                 <div>
                      <label htmlFor="city">City</label>
-                     <input className = 'form-control' type="city" id="city" name="city" value={data.place.city} />
+                     <input className = 'form-control' type="city" id="city" name="city" value={place.city} />
                 </div>
                 <div>
                      <label htmlFor="state">State</label>
-                     <input className = 'form-control' type="state" id="state" name="state" value = {data.place.state} />
+                     <input className = 'form-control' type="state" id="state" name="state" value = {place.state} />
                 </div>
                 <div>
                      <label htmlFor="cuisine">Cuisine</label>
-                     <input className = 'form-control' type="cuisine" id="cuisine" name="cuisine" value={data.place.cuisines}required/>
+                     <input className = 'form-control' type="cuisine" id="cuisine" name="cuisine" value={place.cuisines}required/>
                 </div>
                 <input className = 'btn btn-primary' type = 'submit' value = 'add place'/>
             </form>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,10 +6,10 @@ function show (data) {
         <Def>
           <main>
             <h1>Show Page</h1>
-            <div class="media">
-              <img class="d-flex mr-3" src={data.place.pic} alt="cuisine image"/>
-                <div class="media-body">
-              <h5 class="mt-0 font-weight-bold">{data.place.name}</h5>
+            <div className="media">
+              <img className="d-flex mr-3" src={data.place.pic} alt="cuisine image"/>
+                <div className="media-body">
+              <h5 className="mt-0 font-weight-bold">{data.place.name}</h5>
                Is located in {data.place.city}, {data.place.state}! At this restaurant {data.place.cuisines} cuisine is offered.
                 </div>
             </div>
